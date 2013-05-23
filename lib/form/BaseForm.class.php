ac26bd8c964834cce400cc9c65d6c694aa70242f
bbyte <bbyte@hosting.(none)>
2013-05-24 01:39:35 +0300

    initial comit

---
+++ lib/form/BaseForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base project form.
+ * 
+ * @package    BixGame
+ * @subpackage form
+ * @author     Nikola Kotarov 
+ * @version    SVN: $Id: BaseForm.class.php 20147 2009-07-13 11:46:57Z FabianLange $
+ */
+class BaseForm extends sfFormSymfony
+{
+}
