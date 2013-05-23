a38ec501e067f0935098a347235a2f7d307c3f8b
Nikola Kotarov <bbyte@webhostec.com>
2013-05-24 02:37:18 +0300

    someth

--- apps/frontend/modules/mobile/actions/actions.class.php
+++ apps/frontend/modules/mobile/actions/actions.class.php
@@ -15,8 +15,81 @@ class mobileActions extends sfActions
   *
   * @param sfRequest $request A request object
   */
+
+  public $jsonData = array();
+
+  public function preSubmit(){
+
+    // always talk as json
+
+    $this->getResponse()->setContentType('application/json');
+  }
+
   public function executeIndex(sfWebRequest $request)
   {
-    $this->forward('default', 'module');
+    $this->forward('mobile', 'update');
+  }
+
+  public function executeSetup(sfWebRequest $request){
+
+    $deviceId     = $request->getParameter('deviceId');
+    $deviceType   = $request->getParameter('deviceType');
+    $deviceOs     = $request->getParameter('deviceOs');
+    $deviceToken  = $request->getParameter('deviceToken');
+
+    if(! $deviceId || ! $deviceType || ! $deviceOs){
+
+      $this->jsonData = array("message" => "Missing deviceid, device type or device model!!!");
+      return($this->renderText(json_encode($this->jsonData)));
+    }
+
+    $device = DevicesQuery::filterByDeviceId($deviceId)->findOne();
+
+    if(! $device) {
+      
+      $device = new Devices();
+      $device->setDeviceType($deviceType);
+      $device->setDeviceOs($deviceOs);
+      $device->setDeviceToken($deviceToken);
+      $device->save();
+    }
+    
+    $activity = new Activity();
+    $activity->setDevices($device);
+    $activity->setEvent("SETUP");
+    $activity->save();
+
+    $this->jsonData = array("ok" => TRUE);
+
+    return($this->renderText(json_encode($this->jsonData)));
+  }
+
+  public functoin executeUpdate(sfWebRequest $request){
+  
+    $deviceId = $request->getParameter('deviceId');
+    $event    = $request->getParameter('event');
+
+    if(! $deviceId || ! $event){
+
+      $this->jsonData = array("message" => "Missing device id or event!!!");
+      return($this->renderText(json_encode($this->jsonData)));
+    }
+
+    $device = DevicesQuery::filterByDeviceId($deviceId)->findOne();
+
+    if(! $device){
+
+      $this->jsonData = array("message" => "No such device: ".$deviceId." !!!");
+      return($this->renderText(json_encode($this->jsonData)));
+    }
+
+    $activity = new Activity();
+    $activity->setDevices($device);
+    $activity->setEvent($event);
+    $activity->save();
+
+    $this->jsonData = array("ok" => TRUE);
+
+    return($this->renderText(json_encode($this->jsonData)));
   }
 }
