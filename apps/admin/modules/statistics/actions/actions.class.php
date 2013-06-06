ad50b7ba3cf241a7fd8ae2d6f953d53c45b2648e
Nikola Kotarov <bbyte@webhostec.com>
2013-06-06 03:19:12 +0300

    admin interface

--- apps/admin/modules/statistics/actions/actions.class.php
+++ apps/admin/modules/statistics/actions/actions.class.php
@@ -52,6 +52,73 @@ class statisticsActions extends sfActions
   {
     $this->devices = DevicesQuery::create()->find();
     $this->setupActivities = ActivityQuery::create()->filterByEvent('SETUP')->find();
+
+    $activities = ActivityQuery::create()
+//                                     ->where('Activity.Event = ?', 'STARTED')
+//                                     ->_or()
+//                                     ->where('Activity.Event = ?', 'RESUMED')
+//                                     ->orderByDeviceId()
+                                     ->orderByCreatedAt()
+                                     ->find();
+
+
+    $times = array();
+    $currentDeviceId = 0;
+    $previousDeviceId = 0;
+
+    for ($i = 0; $i < count($activities); $i++)
+    {
+      $activity = $activities[$i];
+      $currentDeviceId = $activity->getDeviceId();
+
+      if (! $previousDeviceId)
+        $previousDeviceId = $currentDeviceId;
+
+      if (! isset($times[$currentDeviceId]))
+      {
+        $times[$currentDeviceId] = array();
+      }
+
+//      else  if ($activity->getEvent() == "STARTED")
+//      {
+//        $times[$currentDeviceId] = array(array());
+//      } else if(!count($times[$currentDeviceId] ))
+//      {
+////        var_dump("dsfsdfdsf");
+//        $times[$currentDeviceId] = array(array());
+//      }
+
+      $index = ((count($times[$currentDeviceId]) == 0) ? 0 : (count($times[$currentDeviceId]) - 1));
+
+//      var_dump($currentDeviceId);
+//      var_dump($index);
+//      var_dump($times[$currentDeviceId]);
+//      var_dump($activity->getCreatedAt());
+//      var_dump($currentDeviceId);
+//      var_dump($previousDeviceId);
+//      echo ("----\n");
+      if ($currentDeviceId != $previousDeviceId)
+      {
+//        $index = count($times[$currentDeviceId]);
+//        $times[$currentDeviceId][$index] = array();
+//        array_push($times[$currentDeviceId], array());
+        $index++;
+      }
+
+      if(! isset($times[$currentDeviceId][$index])){
+        $times[$currentDeviceId][$index] = array();
+      }
+
+      array_push($times[$currentDeviceId][$index], strtotime($activity->getCreatedAt()));
+
+      $previousDeviceId = $currentDeviceId;
+//      var_dump($previousDeviceId);
+//      var_dump($currentDeviceId);
+//      echo ("====\n");
+    }
+
+//    die();
+    $this->times = $times;
   }
 
 }
--- apps/admin/modules/statistics/templates/devicesSuccess.php
+++ apps/admin/modules/statistics/templates/devicesSuccess.php
@@ -9,26 +9,64 @@
 include_once("../vendor/geoip/geoip/geoip.inc");
 include_once("../vendor/geoip/geoip/geoipcity.inc");
 $gi = geoip_open("../GeoLiteCity.dat",GEOIP_STANDARD);
+//echo "<pre>";
+//var_dump($times);
+//echo "</pre>";
 ?>
 
 <table>
   <tr>
+    <td>Device ID</td>
     <td>Device type</td>
     <td>Devce OS version</td>
     <td>Time</td>
     <td>City,Contry</td>
     <td>IP address</td>
+    <td>Number of starts</td>
+    <td>Total time in app</td>
   </tr>
+  <tr><td colspan="8"><hr></td> </tr>
   <?php foreach ($setupActivities as $setupActivity): ?>
     <?php $device = $setupActivity->getDevices() ?>
     <tr>
-    <td><?php echo $device->getDeviceType() ?></td>
-    <td><?php echo $device->getDeviceOs() ?></td>
-    <td><?php echo $device->getCreatedAt() ?></td>
-    <?php $geoipRecord = geoip_record_by_addr($gi,$setupActivity->getIp()) ?>
+      <td><?php echo $device->getId() ?></td>
+      <td><?php echo $device->getDeviceType() ?></td>
+      <td><?php echo $device->getDeviceOs() ?></td>
+      <td><?php echo $device->getCreatedAt() ?></td>
+      <?php $geoipRecord = geoip_record_by_addr($gi, $setupActivity->getIp()) ?>
 
-    <td><?php echo $geoipRecord->city.", ".$geoipRecord->country_name ?></td>
-    <td><?php echo $setupActivity->getIp() ?></td>
+      <td><?php echo $geoipRecord->city . ", " . $geoipRecord->country_name ?></td>
+      <td><?php echo $setupActivity->getIp() ?></td>
+      <td>
+        <?php echo ActivityQuery::create()
+//        ->withColumn('COUNT(Activity.Id)', 'count')
+          ->filterByEvent('STARTED')
+          ->filterByDeviceId($device->getId())
+//        ->groupBy('Activity.DeviceId')
+//        ->having('COUNT(*) > 1', 1)
+          ->find()->count()?>
+      </td>
+      <td>
+        <?php
+          $time = 0;
+
+          $data = $times[$device->getId()]->getRawValue();
+
+          for ($i = (count($data) - 1); $i >= 0; $i--) {
+            $sessionTime = 0;
+            $index = (! (count($data[$i])) ? 0 : (count($data[$i]) - 1));
+            if($index == 0) {
+//              var_dump($data);
+            }
+//            var_dump($index);
+            $sessionTime = $data[$i][$index] - $data[$i][0];
+
+            $time += $sessionTime;
+          }
+
+        ?>
+        <?php echo $time ?>
+      </td>
     </tr>
 
   <?php endforeach ?>
